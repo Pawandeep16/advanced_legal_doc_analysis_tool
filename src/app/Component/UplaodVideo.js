7810316292c35172635c3824188c541384345895
jaspreet singh <jassingh.0244@gmail.com>
2024-09-26 10:25:25 -0400

    Change: file extention and update: upload video file

--- src/app/Component/UplaodVideo.js
+++ src/app/Component/UplaodVideo.js
@@ -1,9 +1,18 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 
 function UplaodVideo() {
+  const filePickerRef = useRef(null);
+
   return (
-    <div className="max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex items-center justify-center text-xl rounded-md">
-      upload video
+    <div className="max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex items-center justify-center rounded-md">
+      <button
+        onClick={() => filePickerRef.current.click()}
+        className="px-2 py-1 text-black bg-white rounded-lg text-xl"
+      >
+        Upload Video
+      </button>
+      <input type="file" className="hidden" ref={filePickerRef} />
     </div>
   );
 }
