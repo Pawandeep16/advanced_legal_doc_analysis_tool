3a19cd9d09a3e363c2e0ae26ed463c2c3164273c
jaspreet singh <jassingh.0244@gmail.com>
2024-09-26 09:02:07 -0400

    first commit

---
+++ src/app/Component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex items-center justify-between px-4 py-2 bg-gradient-to-r from-blue-950 to-white">
+      <h1>DOCINSIGHT</h1>
+      <div>
+        <h2>Login/Signup</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
