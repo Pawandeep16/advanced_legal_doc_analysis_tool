73cfe5a180186e4f274e7df26f5804c65045bfd6
jaspreet singh <jassingh.0244@gmail.com>
2024-09-27 09:27:22 -0400

    fix: svg error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  webpack(config, options) {
+    // Find the existing rule that handles SVG files
+    const fileLoaderRule = config.module.rules.find((rule) => {
+      // Ensure rule.test exists and is a RegExp before calling .test()
+      return rule.test instanceof RegExp && rule.test.test(".svg");
+    });
+
+    if (fileLoaderRule) {
+      // Exclude SVGs from the default file loader to prevent conflicts
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+
+    // Add a new rule to handle SVGs with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
--- src/app/Component/UplaodVideo.js
+++ src/app/Component/UplaodVideo.js
@@ -1,11 +1,14 @@
 "use client";
+import Image from "next/image";
 import React, { useRef } from "react";
+import uploadIcon from "../Assets/icons/uploadtocloud1_114846.webp";
 
 function UplaodVideo() {
   const filePickerRef = useRef(null);
 
   return (
-    <div className="max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex items-center justify-center rounded-md">
+    <div className="max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex flex-col items-center justify-center rounded-md space-y-2">
+      <Image src={uploadIcon} alt="" className="h-12 w-12 text-white" />
       <button
         onClick={() => filePickerRef.current.click()}
         className="px-2 py-1 text-black bg-white rounded-lg text-xl"
