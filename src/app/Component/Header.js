11b2d493e74f1fbbbeea8df58779a2d1f33ecc2f
jaspreet singh <jassingh.0244@gmail.com>
2024-09-27 09:51:57 -0400

    update:login and home page

--- src/app/Component/Header.js
+++ src/app/Component/Header.js
@@ -1,11 +1,21 @@
+"use client";
 import React from "react";
-import Logo from "../Assets/icons/Logo.svg"; // Ensure this path is accurate
+import Logo from "../Assets/icons/Logo.svg";
+import { usePathname, useRouter } from "next/navigation";
+
 function Header() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <div className="flex items-center justify-between px-3 py-6 bg-[#1f3e57]  h-[70px]">
-      <Logo className=" w-[200px] h-10" />
+      <Logo onClick={() => router.push("/")} className=" w-[200px] h-10" />
       <div>
-        <h2 className="text-xl cursor-pointer text-[#F5F5F5]">Login/Signup</h2>
+        <h2
+          onClick={() => router.push("/login")}
+          className="text-xl cursor-pointer text-[#F5F5F5]"
+        >
+          {pathname !== "/login" && "Login / Signup"}
+        </h2>
       </div>
     </div>
   );
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,26 +5,45 @@ import apple from "../Assets/icons/apple.png";
 import fb from "../Assets/icons/fb.png";
 import google from "../Assets/icons/google.webp";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Header from "../Component/Header";
 
 function page() {
   const [signUp, setSignUp] = useState(false);
 
+  const pathname = usePathname();
+
+  console.log(pathname);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-[30%] flex flex-col items-center justify-center space-y-[30px]">
-        <div className="space-y-[15px]">
-          <h1 className="text-5xl text-[#1f3e57] text-center">
-            {signUp ? "Sign Up" : "Login"} to DOCINSIGHT
-          </h1>
-          <p className="text-gray-400 text-[20px] text-center">
-            Welcome Back! Please enter your details
-          </p>
-        </div>
-        <div className="w-full space-y-[20px]">
-          {signUp && (
+    <div>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="w-[30%] flex flex-col items-center justify-center space-y-[30px]">
+          <div className="space-y-[15px]">
+            <h1 className="text-5xl text-[#1f3e57] text-center">
+              {signUp ? "Sign Up" : "Login"} to DOCINSIGHT
+            </h1>
+            <p className="text-gray-400 text-[20px] text-center">
+              Welcome Back! Please enter your details
+            </p>
+          </div>
+          <div className="w-full space-y-[20px]">
+            {signUp && (
+              <div className="space-y-[15px]">
+                <p className="text-[20px] text-[#4f4f4f] font-semibold">
+                  Full Name
+                </p>
+                <input
+                  className="w-full px-[20px] py-3 outline-none rounded-lg border border-gray-500"
+                  type="text"
+                  placeholder="Enter your username or email"
+                />
+              </div>
+            )}
             <div className="space-y-[15px]">
               <p className="text-[20px] text-[#4f4f4f] font-semibold">
-                Full Name
+                Username or Email
               </p>
               <input
                 className="w-full px-[20px] py-3 outline-none rounded-lg border border-gray-500"
@@ -32,32 +51,9 @@ function page() {
                 placeholder="Enter your username or email"
               />
             </div>
-          )}
-          <div className="space-y-[15px]">
-            <p className="text-[20px] text-[#4f4f4f] font-semibold">
-              Username or Email
-            </p>
-            <input
-              className="w-full px-[20px] py-3 outline-none rounded-lg border border-gray-500"
-              type="text"
-              placeholder="Enter your username or email"
-            />
-          </div>
-          <div className="space-y-2">
-            <p className="text-[20px] text-[#4f4f4f] font-semibold">Password</p>
-            <div className="flex items-center justify-between rounded-lg border border-gray-500 pr-[20px]">
-              <input
-                className="w-full pl-[20px] py-3 flex-1 outline-none rounded-lg border-none text-[16px]"
-                type="password"
-                placeholder="Enter your Password"
-              />
-              <Image src={eyeIcon} alt="" className="h-6 w-6 cursor-pointer" />
-            </div>
-          </div>
-          {signUp && (
             <div className="space-y-2">
               <p className="text-[20px] text-[#4f4f4f] font-semibold">
-                Confirm Password
+                Password
               </p>
               <div className="flex items-center justify-between rounded-lg border border-gray-500 pr-[20px]">
                 <input
@@ -72,67 +68,86 @@ function page() {
                 />
               </div>
             </div>
-          )}
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" name="" id="" />
-              <p className="text-[16px] text-[#4f4f4f]">Remember me</p>
+            {signUp && (
+              <div className="space-y-2">
+                <p className="text-[20px] text-[#4f4f4f] font-semibold">
+                  Confirm Password
+                </p>
+                <div className="flex items-center justify-between rounded-lg border border-gray-500 pr-[20px]">
+                  <input
+                    className="w-full pl-[20px] py-3 flex-1 outline-none rounded-lg border-none text-[16px]"
+                    type="password"
+                    placeholder="Enter your Password"
+                  />
+                  <Image
+                    src={eyeIcon}
+                    alt=""
+                    className="h-6 w-6 cursor-pointer"
+                  />
+                </div>
+              </div>
+            )}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" name="" id="" />
+                <p className="text-[16px] text-[#4f4f4f]">Remember me</p>
+              </div>
+              <p className="text-blue-700 text-[16px] font-semibold ml-2 cursor-pointer">
+                Forget Password?
+              </p>
             </div>
-            <p className="text-blue-700 text-[16px] font-semibold ml-2 cursor-pointer">
-              Forget Password?
-            </p>
           </div>
-        </div>
-        {signUp ? (
-          <button className="bg-[#1f3e57] w-full px-1 py-[15px] rounded-lg text-white font-semibold">
-            Sign Up
-          </button>
-        ) : (
-          <button className="bg-[#1f3e57] w-full px-1 py-[15px] rounded-lg text-white font-semibold">
-            Sign In
-          </button>
-        )}
-        {signUp ? (
-          <p className="text-[16px] text-[#4f4f4f]">
-            Already have an account?
-            <span
-              onClick={() => setSignUp(false)}
-              className="text-blue-700 font-semibold ml-2 cursor-pointer"
-            >
-              login
-            </span>
-          </p>
-        ) : (
-          <p className="text-[16px] text-[#4f4f4f]">
-            Don't have an account?
-            <span
-              onClick={() => setSignUp(true)}
-              className="text-blue-700 font-semibold ml-2 cursor-pointer"
-            >
-              Signup
-            </span>
-          </p>
-        )}
-        <div className="space-y-[20px]">
-          <p className="text-center text-[18px] text-[#4f4f4f]">
-            Or Login With
-          </p>
-          <div className="flex items-center space-x-8">
-            <Image
-              src={fb}
-              alt=""
-              className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
-            />
-            <Image
-              src={google}
-              alt=""
-              className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
-            />
-            <Image
-              src={apple}
-              alt=""
-              className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
-            />
+          {signUp ? (
+            <button className="bg-[#1f3e57] w-full px-1 py-[15px] rounded-lg text-white font-semibold">
+              Sign Up
+            </button>
+          ) : (
+            <button className="bg-[#1f3e57] w-full px-1 py-[15px] rounded-lg text-white font-semibold">
+              Sign In
+            </button>
+          )}
+          {signUp ? (
+            <p className="text-[16px] text-[#4f4f4f]">
+              Already have an account?
+              <span
+                onClick={() => setSignUp(false)}
+                className="text-blue-700 font-semibold ml-2 cursor-pointer"
+              >
+                login
+              </span>
+            </p>
+          ) : (
+            <p className="text-[16px] text-[#4f4f4f]">
+              Don't have an account?
+              <span
+                onClick={() => setSignUp(true)}
+                className="text-blue-700 font-semibold ml-2 cursor-pointer"
+              >
+                Signup
+              </span>
+            </p>
+          )}
+          <div className="space-y-[20px]">
+            <p className="text-center text-[18px] text-[#4f4f4f]">
+              Or Login With
+            </p>
+            <div className="flex items-center space-x-8">
+              <Image
+                src={fb}
+                alt=""
+                className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
+              />
+              <Image
+                src={google}
+                alt=""
+                className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
+              />
+              <Image
+                src={apple}
+                alt=""
+                className="h-[40px] w-[40px] rounded-lg bg-[#f1f1f1] p-2 cursor-pointer"
+              />
+            </div>
           </div>
         </div>
       </div>
