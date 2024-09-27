0d1411f02230b6bf4fea6a202e75751d4a0fc3a3
pawandeep16 <thandipawandeep@gmail.com>
2024-09-27 09:29:40 -0400

    addes: Lottie animations

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ pnpm dev
 bun dev
 ```
 
+Lottie animations:
+npm install lottie-web react-lottie
+
+Install SVGR
+npm install @svgr/webpack
+
+
+
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
--- src/app/Component/UplaodVideo.js
+++ src/app/Component/UplaodVideo.js
@@ -1,16 +1,32 @@
 "use client";
 import React, { useRef } from "react";
+import Lottie from 'react-lottie';
+import animationData from '../Assets/aniamtion/cloudupload.json';
+
+
+
 
 function UplaodVideo() {
+  const defaultOptions = {
+    
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   const filePickerRef = useRef(null);
 
+ 
+
   return (
-    <div className="max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex items-center justify-center rounded-md">
+    <div className="  max-w-[80%] mx-auto bg-[#1f3e57] h-[250px] text-white flex items-center justify-center rounded-md flex-col p-10">
+       <Lottie options={defaultOptions} height={400} width={400} />
       <button
         onClick={() => filePickerRef.current.click()}
         className="px-2 py-1 text-black bg-white rounded-lg text-xl"
       >
-        Upload Video
+        Upload Document
       </button>
       <input type="file" className="hidden" ref={filePickerRef} />
     </div>
