f024e7bb305b4eccecd149bdc03752d195303792
jaspreet singh <jassingh.0244@gmail.com>
2024-10-28 14:51:05 -0400

    fix: bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,30 @@
 "use client";
 import Header from './Component/Header';
-import UploadVideo from './Component/UplaodVideo';
-import Topbar from './Component/Topbar.js';
+import UploadVideo from "./Component/UplaodVideo";
+import Topbar from './Component/Topbar';
 import Questions from './Tabs/Questions';
 import Summarization from './Tabs/Summarization';
 import History from './Tabs/History';
 import { useEffect, useState } from 'react';
-import SideBar from "../app/Component/sideBar"
+import SideBar from "../app/Component/sideBar";
 import axios from 'axios';
 export default function Home() {
-
-
   type Chat = {
     question: string;
     answer: string;
   };
-
   const [selected, setSelected] = useState("Summarization");
   const [activeQuestion, setActiveQuestion] = useState("");
   const [summary, setSummary] = useState("");
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
   const [myChat, setMyChat] = useState<Chat[]>([]);
-  const [isOpen, setIsOpen] = useState(false)
-  const [token, setToken] = useState('')
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [token, setToken] = useState<string | null>('');
   const handleSaveChat = () => {
     setActiveQuestion("");
     setFile(null);
   };
-
   const handleFileUpload = async () => {
     if (!file) {
       alert("No file selected");
@@ -41,7 +36,6 @@ export default function Home() {
       const formData = new FormData();
       formData.append("file", file); // Key should match what backend expects
       formData.append("question", activeQuestion); // Replace with actual question if needed
-
       // Send the file as form data to the backend API
       const response = await axios.post(
         "http://localhost:3000/api/summarize",
@@ -52,10 +46,8 @@ export default function Home() {
           },
         }
       );
-
       // Log the complete response for debugging
       console.log("Full response:", response.data.summary);
-
       // Set the returned summary if present
       setSummary(response.data.summary);
       setMyChat((d) => [
@@ -68,15 +60,11 @@ export default function Home() {
       setLoading(false); // Hide loading state
     }
   };
-
   useEffect(() => {
-    const myToken = localStorage.getItem('user')
-    setToken(myToken)
-  }, [])
-
+    const myToken = localStorage.getItem('user');
+    setToken(myToken);
+  }, []);
   console.log(token);
-
-
   const getTab = (selectedTab: string) => {
     switch (selectedTab) {
       case "Q&A":
@@ -104,24 +92,18 @@ export default function Home() {
         return <div>Select a valid tab</div>;
     }
   };
-
-
   const SelectedTab = getTab(selected);
-
   return (
     <div className="scroll-smooth">
       <Header />
-      <div className="flex w-full    ">
-        <div className={`flex-none transition-transform duration-300 mr-2 ${isOpen ? " w-1/6 " : "  "}`} >
+      <div className="flex w-full">
+        <div className={`flex-none transition-transform duration-300 mr-2 ${isOpen ? " w-1/6 " : " "}`}>
           <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
-
         </div>
-        <div className={`mt-5  flex-1 pr-5 space-y-4  ${isOpen ? " w-5/6 " : "   "}`} >
+        <div className={`mt-5 flex-1 pr-5 space-y-4 ${isOpen ? " w-5/6 " : " "}`}>
           <UploadVideo selectedFile={file} setSelectedFile={setFile} loading={loading} />
           <Topbar activeTab={selected} setActiveTab={setSelected} />
-
           {SelectedTab}
-
         </div>
       </div>
     </div>
